9a3b6e705e5b2c8e2d71a893265d0c8592a0889e
Zach Lipps <zachlipps@Zachs-MacBook-Pro.local>
2017-10-11 17:10:27 -0700

    set up template and skeleton topbar

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'react/prefer-stateless-function': 0,
+    'import/no-unresolved': 0
+  }
+};
---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+export default class componentName extends Component {
+  render() {
+    return (
+      <div className="topbar-container">
+        <p>Welcome</p>
+        <p>Intro</p>
+        <p>Portfolio</p>
+        <p>Hobbies</p>
+        <p>Ressume</p>
+        <p>Contact Me</p>
+      </div>
+    );
+  }
+}
