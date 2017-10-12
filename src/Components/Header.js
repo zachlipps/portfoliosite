c5c9bb6c5049e20e1d009ccc49d6386de2d34d4e
Zach Lipps <zachlipps@Zachs-MacBook-Pro.local>
2017-10-12 13:54:07 -0700

    implement scrolling and styled

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 import './Header.css';
 
 export default class Header extends Component {
   render() {
     return (
       <div className="topbar-container">
-        <a href="#welcome">Welcome</a>
-        <a href="#intro">Intro</a>
-        <a href="#projects">Projects</a>
-        <a href="#pobbies">Hobbies</a>
-        <a href="#resume">Resume</a>
-        <a href="#contact-me">Contact Me</a>
+        <AnchorLink href="#welcome">Welcome</AnchorLink>
+        <AnchorLink href="#intro">Intro</AnchorLink>
+        <AnchorLink href="#projects">Projects</AnchorLink>
+        <AnchorLink href="#hobbies">Hobbies</AnchorLink>
+        <AnchorLink href="#resume">Resume</AnchorLink>
+        <AnchorLink href="#contact-me">Contact Me</AnchorLink>
       </div>
     );
   }
