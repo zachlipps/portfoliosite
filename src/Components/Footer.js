1d17734423eb1bb0e04abfe4569003422146748e
Zach Lipps <zachlipps@Zachs-MacBook-Pro.local>
2017-10-13 03:11:31 -0700

    add footer content

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import gitHubLogo from './images/githubLogo.png';
+import linkedInLogo from './images/linkedin.png';
 import './Footer.css';
 
 export default class Footer extends Component {
   render() {
     return (
-      <div className="footer-container" id="contact-me">
-        Footer Imma footer yo
-        <div />
+      <div className="footer">
+        <h1>Contact Me!</h1>
+        <div className="footer-container" id="contact-me">
+          <div className="footer-info">
+            <p>
+              Cell: <a href="tel:6505158151">650-515-8151</a>
+            </p>
+            <p>
+              Email: <a href="mailto:zacharylipps10@gmail.com">zacharylipps10@gmail.com </a>
+            </p>
+          </div>
+          <a href="https://github.com/zachlipps" target="_blank" rel="noopener noreferrer">
+            <img src={gitHubLogo} alt="" className="logo" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/zach-lipps/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedInLogo} alt="" className="logo" />
+          </a>
+        </div>
       </div>
     );
   }
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -10,17 +10,18 @@ export default class Welcome extends Component {
       <div className="welcome-container" id="welcome">
         <Slider />
         <div className="welcome-modal">
-          <h1>Good morning starshine, the earth says Hello!</h1>
+          <h1 className="welcome-statement">Good morning starshine, the earth says Hello!</h1>
           <div className="social-media">
+            <a href="https://github.com/zachlipps" target="_blank" rel="noopener noreferrer">
+              <img src={gitHubLogo} alt="" className="logo" />
+            </a>
+
             <a
               href="https://www.linkedin.com/in/zach-lipps/"
               target="_blank"
               rel="noopener noreferrer"
             >
               <img src={linkedInLogo} alt="" className="logo" />
-            </a>
-            <a href="https://github.com/zachlipps" target="_blank" rel="noopener noreferrer">
-              <img src={gitHubLogo} alt="" className="logo" />
             </a>
           </div>
         </div>
